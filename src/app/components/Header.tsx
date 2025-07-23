8e033972bcb8decfdeaaeaa7f1908407a2ba0096
Don paul <donpaul4444@gmail.com>
2025-07-23 21:50:21 +0530

    name changed

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
         </Link>
         <nav className="space-x-6">
           <Link href="/login" className="text-gray-700 hover:text-blue-600">Login</Link>
-          <Link href="/register" className="text-gray-700 hover:text-blue-600">Register</Link>
+          <Link href="/register" className="text-gray-700 hover:text-blue-600">Registerrrr</Link>
         </nav>
       </div>
     </header>
