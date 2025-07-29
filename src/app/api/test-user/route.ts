d51c764aee33a9d3542f3048cd4c8bdc836df9d0
Don paul <donpaul4444@gmail.com>
2025-07-29 23:27:02 +0530

    tried prisma and postgresql

---
+++ src/app/api/test-user/route.ts
@@ -0,0 +1,9 @@
+// src/app/api/test-user/route.ts
+
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
+}
---
+++ src/app/api/users/route.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { email, name } = await req.json();
+
+  try {
+    const user = await prisma.user.create({
+      data: { email, name },
+    });
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "User creation failed" },
+      { status: 500 }
+    );
+  }
+}
